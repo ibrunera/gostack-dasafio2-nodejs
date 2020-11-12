ed2212a838ff2e2931f28b3ade4e37db694552d8
Bruno T <bteixeira010@gmail.com>
2020-11-12 00:22:26 -0300

    Desafio 02 concluido

--- src/app.js
+++ src/app.js
@@ -1,33 +1,80 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
+const e = require("express");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+function validadeRepositorieId(request, response, next){
+  const {id} = request.params;
+  if(!isUuid(id)) return response.status(400).json({error:'Invalid Repositorie Id.'})
+
+  return next()
+}
+
+app.use("/repositories/:id",validadeRepositorieId);
+
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const repositorie = {id:uuid(), title, url, techs, likes:0};
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie); 
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+  const repositorieIndex = repositories.findIndex((element)=>element.id===id);
+  
+  if(repositorieIndex<0) return response.status(400).json({error: 'Repositorie ID not found.'});
+  
+  const {likes} = repositories[repositorieIndex];
+
+  repositorie = {id, title, url, techs, likes};
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositories[repositorieIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositorieIndex = repositories.findIndex((element)=>element.id===id);
+  
+  if(repositorieIndex<0) return response.status(400).json({error: 'Repositorie ID not found.'});
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositorieIndex = repositories.findIndex((element)=>element.id===id);
+  
+  if(repositorieIndex<0) return response.status(400).json({error: 'Repositorie ID not found.'});
+
+  let {title, url, techs, likes} = repositories[repositorieIndex];
+  likes++;
+  
+  repositorie = {id, title, url, techs, likes};
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositories[repositorieIndex]);
+
 });
 
 module.exports = app;
